17754d7bd3f5d441f89f537baa2357cffdb1b4c9
Anna Artemova <annartmva@gmail.com>
2023-01-17 16:44:10 +0400

    Add header

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 'use strict';
+
+const headerElement = document.querySelector('.header');
+const headerToggleElement = headerElement.querySelector('.header__toggle');
+const navigationElement = headerElement.querySelector('.navigation');
+
+headerToggleElement.addEventListener('click', () => {
+  headerElement.classList.toggle('header--open');
+  navigationElement.classList.toggle('navigation--open');
+});
