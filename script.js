c81d912b30a5a24fe4f3ac6625333b5636f15aa9
Anna Artemova <annartmva@gmail.com>
2023-01-18 23:10:47 +0400

    Add footer

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 'use strict';
 
 const headerElement = document.querySelector('.header');
+const headerLogoElement = headerElement.querySelector('.logo')
 const headerToggleElement = headerElement.querySelector('.header__toggle');
 const navigationElement = headerElement.querySelector('.navigation');
 
 headerToggleElement.addEventListener('click', () => {
   headerElement.classList.toggle('header--open');
+  headerLogoElement.classList.toggle('logo--white');
   navigationElement.classList.toggle('navigation--open');
 });
